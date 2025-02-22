2848ed4ec528ce4f033ba3295542c62dcd198b0d
Ben Denham <ben@denham.nz>
2025-02-22 17:53:45 +1300

    Add ellama config

--- emacs/config/llms.el
+++ emacs/config/llms.el
@@ -9,3 +9,24 @@
   (global-set-key (kbd "C-c a") 'aidermacs-transient-menu)
   (setq aidermacs-auto-commits nil)
   (setq aidermacs-use-architect-mode nil))
+
+(use-package ellama
+  :ensure t
+  :bind ("C-c e" . ellama-transient-main-menu)
+  :init
+  ;; setup key bindings
+  ;; (setopt ellama-keymap-prefix "C-c e")
+  ;; language you want ellama to translate to
+  ;; (setopt ellama-language "German")
+  ;; could be llm-openai for example
+  (require 'llm-openai)
+  (setopt ellama-provider
+          (make-llm-openai-compatible
+           :url "http://localhost:9000"))
+  ;; customize display buffer behaviour
+  ;; see ~(info "(elisp) Buffer Display Action Functions")~
+  (setopt ellama-chat-display-action-function #'display-buffer-full-frame)
+  (setopt ellama-instant-display-action-function #'display-buffer-at-bottom)
+  :config
+  ;; send last message in chat buffer with C-c C-c
+  (add-hook 'org-ctrl-c-ctrl-c-hook #'ellama-chat-send-last-message))
